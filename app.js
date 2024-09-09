75bdfc89fb7318708b8a7502b01d148149b8fbcc
nimesh_dools <nimesh@datanimbus.com>
2024-09-09 23:02:49 +0530

    parse flow logs file

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 const fs = require('fs');
 const readline = require('readline');
 const csv = require('csv-parser');
+const protocols = require('./protocols');
 
 async function parseLookUpFile(lookUpFile) {
 	const results = [];
 	return new Promise((resolve, reject) => {
 		const lookup = {};
-		fs.createReadStream('lookup_table.csv')
-		.pipe(csv())
-		.on('data', (row) => {
-			if (Object.values(row).some(value => value.trim() !== '')) {
-				results.push(row);
-				const key = `${row.dstport}_${row.protocol}`;
-                lookup[key] = row.tag;
-			}
-		})
-		.on('end', () => {
-			resolve(lookup);
-		})
-		.on('error', (error) => {
-			reject(error);
-		});
+		fs.createReadStream(lookUpFile)
+			.pipe(csv())
+			.on('data', (row) => {
+				if (Object.values(row).some(value => value.trim() !== '')) {
+					results.push(row);
+					const key = `${row.dstport}_${row.protocol.toLowerCase()}`;
+					lookup[key] = row.tag.toLowerCase();
+				}
+			})
+			.on('end', () => {
+				resolve(lookup);
+			})
+			.on('error', (error) => {
+				reject(error);
+			});
 	});
+}
+
+async function parseFlowLogsFile(flowLogsFile, lookupTable) {
+	const tagsExistsCount = {};
+	const portProtocolCount = {};
+
+	try {
+		const data = fs.readFileSync(flowLogsFile, 'utf8');
+		const rows = data.trim().split('\n').filter(line => line.trim() !== '');
+
+		rows.forEach(row => {
+			const fields = row.split(' ');
+			const dstport = fields[6];
+			const protocol = protocols[fields[7]];
+			const key = `${dstport}_${protocol.toLowerCase()}`;
+			const tag = lookupTable[key] || 'Untagged';
+
+			if (!tagsExistsCount[tag]) {
+				tagsExistsCount[tag] = 0;
+			}
+			tagsExistsCount[tag]++;
+
+			if (!portProtocolCount[key]) portProtocolCount[key] = 0;
+			portProtocolCount[key]++;
+		});
+
+		return { tagsExistsCount, portProtocolCount };
+	} catch (error) {
+		console.log('ERROR processing flow logs file', error);
+	}
 }
 
 async function main() {
@@ -34,8 +65,10 @@ async function main() {
 		process.exit(1);
 	}
 
-	const lookup = await parseLookUpFile(lookUpFile);
-	console.log(lookup);
+	const lookupTable = await parseLookUpFile(lookUpFile);
+	console.log(lookupTable);
+	const data = await parseFlowLogsFile(flowLogsFile, lookupTable);
+	console.log(data);
 }
 
 main();
